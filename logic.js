75cca04a5fc4c04f007dc4c56a2f1d35c1128e75
Orlando Jose Umanzor Zelaya <orlandojose729@gmail.com>
2022-03-21 12:41:57 -0600

    Create basic UI and logic behind the project
    Create a ui for the project and setup the logic in the
    logic.js file. Add a normal and rainbow mode and the
    color palette for the normal mode. Create the clear
    function that removes all child nodes in the sketch
    container and populates it anew.

---
+++ logic.js
@@ -0,0 +1,80 @@
+d
+function fillColor(e){
+    if(e.buttons != 1) return
+    //Set the background color of the div depending on the mode
+    switch(currentMode){
+        case 'normal':
+            this.style.cssText = `background-color:${colorPalette.value};`
+            break;
+        case 'rainbow':
+            this.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16)
+            break;
+    } 
+}
+function changeColor(){
+    //keep track and change color on label when input is changed
+    backPalette.style.backgroundColor = colorPalette.value
+}
+function changeMode(e){
+    //keeping track of the mode and changing it if necessary
+    currentMode = this.dataset.mode
+    if (this.dataset.mode == 'normal'){
+        normalButton.classList.add('selected')
+        rainbowButton.classList.remove('selected')
+    }
+    else {
+        rainbowButton.classList.add('selected')
+        normalButton.classList.remove('selected')
+    }
+
+}
+
+function clear(e){
+    //clearing the div container and populating it anew
+    while (grid.hasChildNodes()) {  
+        grid.removeChild(grid.firstChild);
+      }
+    if(e) setupGrid(gridSize.value)
+}
+function setupGrid(size){
+    //creating the grid layout and populating it
+    clear();
+    changeColor();
+    grid.style.cssText = `grid-template : repeat(${size},1fr) / repeat(${size},1fr)`
+    sliderPara.textContent = `${size} ✗ ${size}`
+    
+    for(let i = 0;i<size*size;i++){
+        let div = document.createElement('div')
+        div.addEventListener('mouseover', fillColor)
+        div.addEventListener('mousedown', fillColor)
+        grid.appendChild(div)
+    }
+}
+
+
+const defaultSize = 16;
+let currentMode = 'normal';
+
+const backPalette = document.querySelector('label')
+const sliderPara = document.querySelector('.slider-container')
+const colorPalette = document.querySelector('#colorPalette')
+const gridSize = document.querySelector('.slider')
+const grid = document.querySelector('.sketch')
+const clearButton = document.querySelector('.clear')
+const normalButton = document.querySelector('.normal')
+const rainbowButton = document.querySelector('.rainbow')
+
+rainbowButton.addEventListener('click', changeMode)
+normalButton.addEventListener('click', changeMode)
+colorPalette.addEventListener('change', changeColor)
+clearButton.addEventListener('click', clear)
+gridSize.addEventListener('change',()=>{setupGrid(gridSize.value)})
+
+
+
+//Setup the basic when loading the page
+window.onload = () =>{
+    setupGrid(defaultSize)
+}
+
+
